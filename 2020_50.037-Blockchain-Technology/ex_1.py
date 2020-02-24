0a191d31f795eeed91cbd192ddf8df33e73d74a5
Ainul-MdRazib <ainul_mardhiyyah@mymail.sutd.edu.sg>
2020-02-24 18:39:40 +0800

    Update with week 4 material

--- 2020_50.037-Blockchain-Technology/ex_1.py
+++ 2020_50.037-Blockchain-Technology/ex_1.py
@@ -33,7 +33,154 @@ for num in range(1, 6):
     print("{} - collision found after {} seconds".format(num, str(end - start)))
 
 print("complete)")
+
+print("Preimage is some x' such that hashvalue = h(x')")
+hash_of_preimages = [b"\x00", b"\x00"*2, b"\x00"*3, b"\x00"*4, b"\x00"*5]
+for num in range(1, 6):
+    start, end, msg = time.time(), None, 0
+    while end == None:
+        hashval = H(num, str(msg))
+        if (hashes.get(hashval) 
+            and hashval in hash_of_preimages):
+            end = time.time()
+        else:
+            hashes[hashval] = str(msg)
+            msg += 1
+    print("{} - preimage found after {} seconds".format(num, str(end - start)))
+
+## Finding a preimage takes much longer than finding a collision
+## because while many inputs can generate the same hash, the converse is true vice versa
+
+
+"""
+## Q3 ##   
+
+Generate key pairs for ECDSA and sign the string `"Blockchain Technology"`
+using this signature scheme with the generated key. Then verify the obtained
+signature.
+
+**Note:** the `ecdsa` package uses NIST192p as the default elliptic curve, but in
+real applications a longer/more secure curve should be considered.
+
+
+import ecdsa
+
+signing_key = ecdsa.SigningKey.generate()
+verifying_key = signing_key.get_verifying_key()
+
+signature = signing_key.sign(b"Blockchain Technology")
+print(verifying_key.verify(signature, b"Blockchain Technology"))
+
+
+## Q4 ##
+from datetime import datetime
+import json
+from ecdsa import VerifyingKey
+ 
+class Transaction:
+    def __init__(self, sender, receiver, amount, comment):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+        self.comment = comment
+        self.signature = None
+
+    @classmethod
+    def new(cls, sender, receiver, amount, comment, signature = None):
+        # Instantiates object from passed values
+        tx = cls(sender, receiver, amount, comment)
+        senderPrivKey = sender.get_verifying_key()
+        tx.sign(False)
+
+    def serialize(self, isSigned = True):
+        """
+        Serializes transaction information into JSON
+        """
+        tx = {
+            "senderKey": self.sender.to_string().hex(),
+            "receiverKey": self.receiver.to_string().hex(),
+            "amount": self.amount,
+            "comment": self.comment,
+        }
+
+        if isSigned:
+            tx["signature"] = self.signature.hex()
+
+        return json.dumps(tx)
+
+    @classmethod
+    def deserialize(cls, obj):
+        # Instantiates/Deserializes object from CBOR or JSON string
+        deserialized = json.loads(obj)
+
+        senderKey = VerifyingKey.from_string(bytes.fromhex(deserialized["senderKey"]))
+        receiverKey = VerifyingKey.from_string(bytes.fromhex(deserialized["receiverKey"]))
+        amount = deserialized["amount"]
+        comment = deserialized["comment"]
+
+        signature = bytes.fromhex(deserialized["signature"]) if deserialized["signature"] else None
+
+        tx = cls(senderKey, receiverKey, amount, comment)
+        tx.signature = signature
+
+        if tx.validate():
+            return tx
+        elif not signature:
+            raise Exception("ValidationError: Signature cannot be verified")
+        else:
+            raise Exception("ValidationError: Signature cannot be found")
+
+
+    def sign(self, private_key):
+        # Sign object with private key passed
+        # That can be called within new()
+        # just use ecdsa
+        s = self.serialize(False)
+        new_sign = private_key.sign(s.encode())
+        self.signature = new_sign
         
 
+    def validate(self):
+        # Validate transaction correctness.
+        # Can be called within from_json()
+        # check signature = 
+        sig = self.signature
+        s = self.serialize(False)
+        return self.sender.verify(sig, s.encode())
 
+    def __eq__(self, other):
+        # Check whether transactions are the same
+        if (self.sender == other.sender
+            and self.receiver == other.receiver
+            and self.signature == other.signature
+            and self.comment == other.comment
+            and self.amount == other.amount):
+            return True
+        else:
+            return False
+
+"""
+
+class MerkleTree(...):
+    ...
+
+    def add(...):
+        # Add entries to tree
+        ...
+
+    def build(...):
+        # Build tree computing new root
+        ...
+
+    def get_proof(...):
+        # Get membership proof for entry
+        ...
+
+     def get_root(...):
+        # Return the current root
+        ...
+
+def verify_proof(entry, proof, root):
+    # Verify the proof for the entry and given root. Returns boolean.
+    ...
 
